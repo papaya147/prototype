dd15efb57959f5569a6e0ca4018aef917982b56d
Mark Dayton <46218216+papaya147@users.noreply.github.com>
2023-08-11 13:17:19 +0530

    reverted ssl

--- front-end/App.js
+++ front-end/App.js
@@ -16,7 +16,7 @@ class App extends Component {
 
   componentDidMount() {
     // Replace 'http://your-socket-server-url' with your actual WebSocket server URL
-    const socket = socketIOClient('ws://13.200.109.200:4000');
+    const socket = socketIOClient('ws://3.111.94.212:4000');
 
     socket.on('connect', () => {
       console.log('Connected to server');
--- real-time-server/src/index.ts
+++ real-time-server/src/index.ts
@@ -1,18 +1,8 @@
 import { Server } from 'socket.io'
 import { IMessage, KafkaService } from './kafka-service'
-import fs from 'fs'
-import https from 'https'
-
-const options = {
-    key: fs.readFileSync('privatekey.pem'),
-    cert: fs.readFileSync('certificate.pem')
-}
-
-// create https server
-const server = https.createServer(options)
 
 // create socket to receive data
-const io = new Server(server)
+const io = new Server(4000)
 console.log('Socket created at port 4000')
 
 io.on('connection', (socket) => {
@@ -26,8 +16,4 @@ io.on('connection', (socket) => {
 
         socket.emit('ack', JSON.stringify(data))
     })
-})
-
-server.listen(4000, () => {
-    console.log('Server is running on port 4000');
 })
